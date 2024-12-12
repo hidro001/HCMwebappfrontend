3c71ccdc2c8a1e3686f14b3f31899c33a31abec6
Amitk2108 <amitcoder98@gmail.com>
2024-12-12 18:43:01 +0530

    Testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Login from "./components/login/Login";
+
 export default function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world Welcome New Ui!
+      <h1>
+        <Login />
       </h1>
     </>
   );
