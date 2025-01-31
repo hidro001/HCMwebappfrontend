fbde17822e567913db668ddf5c74be7199c5a62e
amit <amitcoder98@gmail.com>
2025-01-31 18:46:14 +0530

    instance update

--- src/service/axiosInstance.js
+++ src/service/axiosInstance.js
@@ -4,8 +4,7 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   baseURL:import.meta.env.VITE_API_BASE_URL,
   headers: {
-    // 'Content-Type': 'application/json',
-    'Content-Type': 'multipart/form-data',
+    'Content-Type': 'application/json',
   },
 });
 
