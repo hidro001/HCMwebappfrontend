5d221925561237065b201ff90cf8cf37483559b1
rohit <hiddrodox8989@gmail.com>
2025-05-29 12:37:09 +0530

    complete video functionality

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,3 @@
-// import { createRoot } from "react-dom/client";
-// import "./index.css";
-// import App from "./App.jsx";
-
-// createRoot(document.getElementById("root")).render(<App />);
-
-// src/index.js
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -13,24 +6,20 @@ import { CallProvider } from "./contexts/CallContext.jsx";
 import CallModal from "./components/chats/calls/CallModal.jsx";
 import CallWindow from "./components/chats/calls/CallWindow.jsx";
 import VideoCallModal from "./components/chats/calls/VideoCallModal.jsx";
-
-// import { ChatProvider } from "./contexts/ChatContext";
 import { ChatProviderv2 } from "./contexts/ChatContextv2.jsx";
 
 
+const employee = localStorage.getItem("employeeId");
+console.log("Employee ID:", employee);
+
 createRoot(document.getElementById("root")).render(
   <ThemeProvider>
-    <CallProvider>
+    <CallProvider currentUserId={employee}>
       <CallModal />
       <CallWindow />
       <VideoCallModal />
       <ChatProviderv2>
-
-    
-      {/* <ChatProvider> */}
-     
         <App />
-      {/* </ChatProvider> */}
       </ChatProviderv2>
     </CallProvider>
   </ThemeProvider>
